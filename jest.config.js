eadc44dcf04827ffad19c4ec0c28624f4c41798c
Vit Bokisch <vit@bokisch.cz>
2018-10-08 19:18:25 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
   snapshotSerializers: ['enzyme-to-json/serializer'],
   roots: ['<rootDir>./src/'],
   setupTestFrameworkScriptFile: './tools/jest/config.ts'
--- tools/jest/config.ts
+++ tools/jest/config.ts
@@ -1,4 +1,21 @@
-import * as Enzyme from 'enzyme'
-import * as Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      React: any;
+      testFor: any;
+      MSG_RENDERER: any;
+    }
+  }
+}
+
+declare var MSG_RENDERER: any
+declare const testFor: any
+
+global.testFor = (component: any, callback: any) =>
+  describe('\r\n' + component.displayName + '\r\n', callback)
+global.MSG_RENDERER = '-- COMPONENT --'
 
 Enzyme.configure({ adapter: new Adapter() })
