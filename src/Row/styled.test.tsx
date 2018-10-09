ca33714f30a7140ec202bac4eb2b884a87806aa9
Vit Bokisch <vit@bokisch.cz>
2018-10-09 12:58:30 +0200

    Row - add styled component test

--- src/Row/styled.test.tsx
+++ src/Row/styled.test.tsx
@@ -6,5 +6,26 @@ describe('Row - styled component', () => {
   it('renders correctly', async () => {
     const tree = shallow(<Component />)
     expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('box-sizing', 'border-box')
+  })
+
+  it('renders correctly with extendCss', async () => {
+    const extendCss = 'font-size: 20px; background-color: black'
+    const tree = shallow(<Component extendCss={extendCss} />)
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('font-size', '20px')
+    expect(tree).toHaveStyleRule('background-color', 'black')
+  })
+
+  it('renders correctly with gap', async () => {
+    const tree = shallow(<Component gap={20} />)
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('margin', '10px -10px')
+  })
+
+  it('renders correctly with gap and gutter', async () => {
+    const tree = shallow(<Component gap={20} gutter={0} />)
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('margin', '-10px -10px')
   })
 })
