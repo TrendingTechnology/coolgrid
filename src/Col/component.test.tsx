ae012529a98eab99ceb97f4428057dfa1d528a81
Vit Bokisch <vit@bokisch.cz>
2018-10-09 12:45:37 +0200

    Col - add styled component tests

--- src/Col/component.test.tsx
+++ src/Col/component.test.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { shallow } from 'enzyme'
 import Component from '.'
 
-global.testFor(Component, () => {
-  describe(MSG_RENDERER, () => {
-    it('renders correctly', async () => {
+describe(Component.displayName, () => {
+  describe(TEST_COMPONENT, () => {
+    it('component renders correctly', async () => {
       const tree = shallow(<Component />)
       expect(tree).toMatchSnapshot()
     })
