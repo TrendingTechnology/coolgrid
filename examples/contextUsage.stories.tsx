d98c4ca7845612b84a21240e9c21d60d0c9b1948
Vit Bokisch <vit@bokisch.cz>
2018-11-07 17:01:35 +0100

    add custom context usage (would be great to improve it later)

---
+++ examples/contextUsage.stories.tsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Container from '../src/Container'
+import Row from '../src/Row'
+import Col from '../src/Col'
+
+const columnCss = `
+  background-color: #e0e0eb;
+  border: 1px solid #b3b3cc;
+  padding: 20px;
+`
+
+const columnCss1 = `
+  background-color: #efefef;
+  border: 1px solid #e0e0e0;
+  padding: 20px;
+`
+
+storiesOf('Context usage', module)
+  .add('Define gap and size on Container', () => {
+    return (
+      <Container gap={10} size={4} extendColCss={columnCss}>
+        <Row>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+        </Row>
+        <Row>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+        </Row>
+      </Container>
+    )
+  })
+  .add('Define gap and size on Rows', () => {
+    return (
+      <Container extendColCss={columnCss}>
+        <Row gap={10} size={4}>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+        </Row>
+        <Row gap={20} size={3}>
+          <Col>gap-20</Col>
+          <Col>gap-20</Col>
+          <Col>gap-20</Col>
+          <Col>gap-20</Col>
+        </Row>
+      </Container>
+    )
+  })
+  .add('Different Col styles', () => {
+    return (
+      <Container extendColCss={columnCss}>
+        <Row>
+          <Col>gap-0</Col>
+          <Col>gap-0</Col>
+          <Col>gap-0</Col>
+          <Col>gap-0</Col>
+          <Col>gap-0</Col>
+        </Row>
+        <Row gap={10} extendColCss={columnCss1}>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+          <Col>gap-10</Col>
+        </Row>
+        <Row gap={20}>
+          <Col>gap-20</Col>
+          <Col>gap-20</Col>
+          <Col>gap-20</Col>
+          <Col>gap-20</Col>
+          <Col>gap-20</Col>
+        </Row>
+        <Row gap={20} extendColCss={columnCss1}>
+          <Col>gap-30</Col>
+          <Col>gap-30</Col>
+          <Col>gap-30</Col>
+          <Col>gap-30</Col>
+          <Col>gap-30</Col>
+        </Row>
+        <Row gap={40}>
+          <Col>gap-40</Col>
+          <Col>gap-40</Col>
+          <Col>gap-40</Col>
+          <Col>gap-40</Col>
+          <Col>gap-40</Col>
+        </Row>
+      </Container>
+    )
+  })
