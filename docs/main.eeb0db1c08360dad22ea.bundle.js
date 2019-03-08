202b96cf45da65c91c35c06165adf2d2bf473f96
Vit Bokisch <vit@bokisch.cz>
2019-03-08 14:00:16 +0100

    generate new docs

--- docs/main.eeb0db1c08360dad22ea.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(344),p(425),n.exports=p(581)},425:function(n,o,p){"use strict";p.r(o);p(231),p(570),p(575)}},[[0,1,2]]]);
---
+++ doczrc.js
@@ -0,0 +1,4 @@
+export default {
+  files: '**/*.{md,markdown,mdx}',
+  dest: '/docs'
+}
