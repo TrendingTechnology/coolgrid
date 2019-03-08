3eeb110b5cfa5bced6334f28d5be50bb353108fa
Vit Bokisch <vit@bokisch.cz>
2019-03-08 23:00:20 +0100

    update docz commands

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,6 @@
 export default {
   base: '/coolgrid/',
-  title: 'Coolstyle - the only grid you need',
+  title: 'Coolstyle',
   description: 'The most flexible grid ever',
   files: '**/*.{md,markdown,mdx}',
   dest: '/docs'
